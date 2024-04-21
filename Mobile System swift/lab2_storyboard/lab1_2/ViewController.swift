297d758f787eac3c760b35aae63b4457ca6a1d08
prostakkk <110781708+prostakkk@users.noreply.github.com>
2024-04-21 21:25:40 +0300

    Add files via upload

---
+++ Mobile System swift/lab2_storyboard/lab1_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab1_2
+//
+//  Created by Admin on 2/25/24.
+//  Copyright © 2024 pk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mobile System swift/lab2_storyboard/lab1_2Tests/lab1_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lab1_2Tests.swift
+//  lab1_2Tests
+//
+//  Created by Admin on 2/25/24.
+//  Copyright © 2024 pk. All rights reserved.
+//
+
+import XCTest
+@testable import lab1_2
+
+class lab1_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
